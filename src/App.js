2ac63e7b03567e65a4eeaffac1b3a06418d0c1dc
cotyadams <cotyadams1@gmail.com>
2025-01-08 17:33:34 -0500

    all comments added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import NoSleep from 'nosleep.js';
 
 function App() {
+  // create a new NoSleep object
   var noSleep = new NoSleep();
 
+  // toggle no sleep functionality based on the NoSleep object
   const toggleSleep = () => {
     if (!noSleep.enabled) {
       noSleep.enable();
@@ -15,6 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>NoSleep Page</h1>
+        {/* creates the switch box*/}
         <label class="switch">
           <input type="checkbox" onClick={toggleSleep}/>
           <span class="slider round"></span>
